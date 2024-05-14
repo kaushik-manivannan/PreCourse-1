057915a0f37a64580b704409be996a5460c892b3
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-05-14 14:33:13 +0530

    Complete Exercise 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,42 @@
+# Time Complexity : O(1) for isEmpty(), push(), pop(), peek(), size(), and show()operations
+# Space Complexity : O(1) for isEmpty(), push(), pop(), peek(), size(), and show() operations
+# Any problem you faced while coding this : No
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+
+  # Initializes an empty array and the length of the array
+  def __init__(self):
+    self.arr = []
+    self.length = 0
+  
+  # Checks if the array is empty
+  def isEmpty(self):
+    return True if len(self.arr) == 0 else False
+  
+  # Adds an item to the end of the array
+  def push(self, item):
+    self.arr.append(item)
+    self.length += 1
+      
+  # Removes an item from the end of the array and returns the removed item
+  def pop(self):
+    if self.length > 0:
+      self.length -= 1
+      return self.arr.pop()
+    else:
+      return "Cannot pop from an empty array!"
+    
+  # Returns the item at the end of the array
+  def peek(self):
+    return self.arr[-1]
+  
+  # Returns the current size of the array
+  def size(self):
+    return self.length
+  
+  # Returns the current array
+  def show(self):
+    return self.arr
 
 s = myStack()
 s.push('1')
