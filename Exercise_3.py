f3d4333a545edf38de11c4b3831b7884a0141f61
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-05-14 16:37:56 +0530

    Complete Exercise 3

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,8 +3,12 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
+
+    # Initializes the head of the linked list
     def __init__(self):
         """
         Create a new singly-linked list.
@@ -17,6 +21,16 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        newNode = ListNode(data)
+        # Checks if the linked list contains atleast one node
+        if self.head:
+            currNode = self.head # Sets the current node to point to the head node
+            # Iterates until it finds the tail of the linked list
+            while currNode.next:
+                currNode = currNode.next
+            currNode.next = newNode # Sets the next pointer of the tail to point to the new node
+        else:
+            self.head = newNode # Sets the head of the empty linked list to point to the new node
         
     def find(self, key):
         """
@@ -24,9 +38,35 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        # Checks if the linked list contains atleast one node
+        if self.head:
+            currNode = self.head # Assigns currNode to the head of the linked list
+            while currNode:
+                # Checks if the data of the current node matches the given key
+                if currNode.data == key:
+                    return currNode
+                currNode = currNode.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head:
+            prevNode = None
+            currNode = self.head # Assigns currNode to point to the head of the linked list
+            while currNode:
+                nextNode = currNode.next if currNode.next else None
+                # Checks if the data of the current node matches the given key
+                if currNode.data == key:
+                    if not prevNode:
+                        self.head = nextNode
+                        return
+                    else:
+                        prevNode.next = nextNode
+                        return
+                prevNode = currNode
+                currNode = nextNode
+            self.head = None
+        return "Key is not present in the list!"
