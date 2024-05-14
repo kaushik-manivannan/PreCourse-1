cbba8599f1a3fa05f3cba06cdea944b7bef3b84b
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-05-14 15:11:06 +0530

    Complete Exercise 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,6 @@
+# Time Complexity : O(1) for push() and pop() operations
+# Space Complexity : O(1) for push() and pop() operations
+# Any problem you faced while coding this : No
 
 class Node:
     def __init__(self, data):
@@ -5,11 +8,37 @@ class Node:
        self.next = None
  
 class Stack:
+    
+    # Initializes the head of the linked list
     def __init__(self):
-        
+        self.head = None
+    
+    # Adds a node to the beginning of the linked list
     def push(self, data):
+        # Checks if the linked list is empty
+        if not self.head:
+            self.head = Node(data)
+        # Checks if the linked list contains atleast one value
+        else:
+            newNode = Node(data) # Creates a node with the data
+            newNode.next = self.head # Sets the next pointer of the new node to point to the current head node
+            self.head = newNode # Sets the new node as the head of the linked list
         
+    # Removes a node from the beginning of the linked list and return the data of the removed node
     def pop(self):
+        # Checks if the linked list contains more than one value
+        if self.head and self.head.next:
+            poppedValue = self.head.data
+            self.head = self.head.next # Sets the new head node to be the next node of the current head node
+            return poppedValue
+        # Checks if the linked list contains only one value
+        elif self.head and not self.head.next:
+            poppedValue = self.head.data
+            self.head = None # Sets the head node to None
+            return poppedValue
+        # Checks if the linked list is empty
+        else:
+            return None
         
 a_stack = Stack()
 while True:
